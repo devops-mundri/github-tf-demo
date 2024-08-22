73ad65c38a302cbe5a2803170ea763489dc933cb
devops-mundri <152005289+devops-mundri@users.noreply.github.com>
2024-08-22 11:29:51 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
-output "bucket_arn" {
-  value = aws_s3_bucket.my_bucket.arn
+output "instance_id" {
+  description = "The ID of the AWS instance"
+  value       = aws_instance.example.id
+}
+
+output "instance_public_ip" {
+  description = "The public IP address of the AWS instance"
+  value       = aws_instance.example.public_ip
 }
